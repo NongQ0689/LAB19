c944a9c95f690aa2bcc515b7c1ccac59c1e53a4f
Suraphat Baroonphan <suraphat.q@gmail.com>
2022-01-25 18:54:29 +0700

    finish (ทำจนปวดหลัง)

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -16,23 +16,70 @@ char score2grade(int score){
 
 string toUpperStr(string x){
     string y = x;
-    for(unsigned i = 0; i < x.size();i++) y[i] = toupper(x[i]);
+    for(unsigned int i = 0; i < x.size();i++) y[i] = toupper(x[i]);
     return y;
 }
 
-void importDataFromFile(){
+void importDataFromFile(string filename , vector<string> &names ,vector<int> &scores , vector<char> &grades)
+{
+    ifstream source(filename);
+    string input ;
+    char format[] = "%[^:]: %f %f %f";
+    char scanname[50] ;
+    float a,b,c;
+    while (getline(source,input))
+    {
+        const char *text = input.c_str();
+        sscanf(text,format,scanname,&a,&b,&c);
 
+        names.push_back(scanname);
+        scores.push_back(a+b+c);
+        grades.push_back(score2grade(a+b+c));
+    }
 }
 
-void getCommand(){
+void getCommand(string &command, string &key){
 
+    cout<<"Please input your command: ";
+
+    string input;
+    getline(cin,input);
+    const char *text = input.c_str();
+    char format[] = "%s %[^:] ";
+    char cm[20] , k[20] ;
+    sscanf(text,format,&cm,&k);
+    command = cm;
+    key = k; 
 }
 
-void searchName(){
-
+void searchName(vector<string> &names , vector<int> &score ,vector<char> &grade , string &key){
+    cout<<"---------------------------------\n";
+    bool find ;
+    for(unsigned int i = 0 ; i < names.size(); i++)
+    {
+        if(toUpperStr(names[i])==key)
+        {
+            cout<<names[i]<<"'s "<<"score = "<<score[i]<<"\n";
+            cout<<names[i]<<"'s "<<"grade = "<<grade[i]<<"\n";
+            find = true;
+        }
+    }
+    if(!find) cout<<"Cannot found.\n";
+    cout<<"---------------------------------\n";
 }
 
-void searchGrade(){
+void searchGrade(vector<string> &names , vector<int> &score ,vector<char> &grade , string &key){
+    cout<<"---------------------------------\n";
+    const char *constcharkey = key.c_str();
+    char KEY = *constcharkey;
+    for(unsigned int i =0 ; i < names.size(); i++)
+    {
+        if(grade[i]==KEY)
+        {
+            cout<<names[i]<<" ("<<score[i]<<")\n";
+        }
+    }
+    cout<<"---------------------------------\n";
 
 }
 
